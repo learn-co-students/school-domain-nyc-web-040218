29346582cee065b621ddcd5ce49b7959eedb55ab
EJ <EJ@tests-MacBook-Pro-3.local>
2018-04-06 16:30:10 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+
+class School
+
+attr_accessor :roster
+
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+
+def add_student (student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name    
+    
+end 
+
+def grade(grade)
+    @roster[grade]
+end 
+
+
+def sort
+#    @roster.each_pair {|key, value|value.sort}
+   @roster.each {|key, value| value.sort! }
+
+
+end 
+
+
+    # value of each key will be an array of student names
+
+
+end
+
+
+#School.new("Long Reach HS")
+
