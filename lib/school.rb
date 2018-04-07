8b3611e70cd0c46dcb93c62f79bfe988c76cc6ed
AustinPaley <austinpaley32@gmail.com>
2018-04-07 19:27:15 -0400

    Completed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+class School
+
+attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(their_grade)
+    @roster[their_grade]
+    # @roster
+  end
+
+  def sort
+    # binding.pry
+    updated_roster = {}
+    @roster.each do |grades, students|
+      updated_roster[grades] = students.sort
+    end
+    # new_keys = @roster.keys.sort
+    # new_values = @roster.values.sort
+    # updated_roster[new_keys] = new_values
+    updated_roster
+  end
+end
+
+#School #sort is able to sort the students
